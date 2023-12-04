40fc923061ffe77a7ab733e2d2d338182b890151
St.Sergey.Minsky <Sterlikovsergey@icloud.com>
2023-12-04 17:49:36 +0300

    Initial commit

---
+++ src/main/java/st/sergey/minsky/shop2doordelivers/Shop2DoorDeliversApplication.java
@@ -0,0 +1,13 @@
+package st.sergey.minsky.shop2doordelivers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop2DoorDeliversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop2DoorDeliversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/st/sergey/minsky/shop2doordelivers/Shop2DoorDeliversApplicationTests.java
@@ -0,0 +1,13 @@
+package st.sergey.minsky.shop2doordelivers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop2DoorDeliversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
